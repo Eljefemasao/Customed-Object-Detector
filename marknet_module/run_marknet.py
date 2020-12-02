89895e6213845199fde9f510dcdd08fa4bfbee49
m.matsunaga <castle2164m@gmail.com>
2020-12-02 18:20:29 +0900

    clean up

--- marknet_module/run_marknet.py
+++ marknet_module/run_marknet.py
@@ -58,7 +58,8 @@ history = {
     'test_acc': [],
 }
 
-for epoch in range(10):  # loop over the dataset multiple times
+Epoch = 30 
+for epoch in range(Epoch):  # loop over the dataset multiple times
 
     running_loss = 0.0
     for i, data in tqdm(enumerate(trainloader, 0)):
@@ -85,8 +86,9 @@ for epoch in range(10):  # loop over the dataset multiple times
         # print statistics
         running_loss += loss.item()
         if i % 200 == 199:    # print every 2000 mini-batches
-            print('[%d, %5d] loss: %.3f' %
+            print('Epoch: [%d, %5d] loss: %.3f' %
                   (epoch + 1, i + 1, running_loss / 200))
+            
             running_loss = 0.0
             history['train_loss'].append(loss)
 
