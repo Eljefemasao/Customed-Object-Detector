a8fc60b10ac589c2daca294d0f01a924edc3a498
m.matsunaga <castle2164m@gmail.com>
2020-12-03 01:46:03 +0900

    add attention branch network

--- marknet_module/model/MarkNet_keras.py
+++ marknet_module/model/MarkNet_keras.py
@@ -8,7 +8,14 @@ from keras.layers import Dropout
 from keras.models import Model
 import keras 
 
+# attentionで使用
+from keras.layers import GlobalAveragePooling2D
+from keras.layers import BatchNormalization
+from keras.layers import Multiply
+from keras.layers import Add
+from keras.layers import concatenate
 
+import numpy as np
 
 class Model_:
 
@@ -20,7 +27,11 @@ class Model_:
 
         # モデル
         net2 = {}
-        
+        # Attention Branch
+        att = {}
+        # Attention マップ
+        att_map = {}
+
         inputs = Input(shape=self.input_shape)
         
         net2['input_m'] = inputs
@@ -38,22 +49,55 @@ class Model_:
                                    border_mode='same',
                                    name='pool1m')(net2['conv1_2m'])
 
+
         net2['drop1m'] = Dropout(0.25,
                                 name='drop1m')(net2['pool1m'])
 
-        net2['conv2_1m'] = Convolution2D(64, 3,3, # kernel was 128 for including to ssd
+
+        net2['conv2_1m'] = Convolution2D(5, 3,3, # kernel was 128 for including to ssd
                                         activation='relu',
                                         border_mode='same',
                                         name='conv2_1m')(net2['drop1m'])
         
-        net2['conv2_2m'] = Convolution2D(64, 3,3, # kernel was 256 for including to ssd
-                                        activation='relu',
-                                        border_mode='same',
-                                        name='conv8_2')(net2['conv2_1m']) # previous 64ch
 
         net2['pool2m'] = MaxPooling2D((2,2),
                                    border_mode='same',
-                                   name='pool2m')(net2['conv2_2m'])
+                                   name='pool2m')(net2['conv2_1m'])
+
+
+
+        ### Attention Branch
+
+        att['batch_norm'] = BatchNormalization(name='batch_norm')(net2['pool2m'])
+        att['conv1_att'] = Convolution2D(16, 1,1,
+                                       activation='relu',
+                                       border_mode='same',
+                                       name='conv1_att')(att['batch_norm'])
+        att['conv2_att'] = Convolution2D(5, 1, 1,
+                                         activation='relu',
+                                         border_mode='same',
+                                         name='conv2_att')(att['conv1_att'])
+
+        att['gap'] = GlobalAveragePooling2D(name='gap')(att['conv2_att'])
+        att['softmax'] = Activation(activation='softmax', name='attention_branch')(att['gap'])
+
+        print('attention_branch:', np.shape(att['softmax']))
+        ### Attention Map
+
+        att_map['map'] = Convolution2D(5,1,1,
+                                       border_mode='same',
+                                       name='map')(att['conv1_att'])
+
+        att_map['batch_norm'] = BatchNormalization(name='batch_norm_map')(att_map['map'])
+        att_map['sigmoid'] = Activation(activation='sigmoid')(att_map['batch_norm'])
+
+        ### Connection
+
+        print("sssssssssssssssss",np.shape(net2['pool2m']))
+        print(np.shape(att_map['sigmoid']))
+        net2['marge'] = Multiply()([net2['pool2m'], att_map['sigmoid']])
+        net2['add_marge'] = Add()([net2['marge'], net2['pool2m']])
+
         ###
       #  model.add(Conv2D(512, (3, 3), name='conv6_2')) #元々64ch
       #  model.add(Activation('relu'))
@@ -61,17 +105,22 @@ class Model_:
         ###
 
         net2['drop2m'] = Dropout(0.25,
-                                name='drop2m')(net2['pool2m'])
+                                name='drop2m')(net2['add_marge'])
         
+
         net2['flat1m'] = Flatten(name='flat1m')(net2['drop2m'])
         net2['dense1m'] = Dense(512, name='dense1m', activation='relu')(net2['flat1m'])
 
         net2['drop3m'] = Dropout(0.5,
                                 name='drop3m')(net2['dense1m'])
         
-        net2['dense2m'] = Dense(5, name='dense2m', activation='softmax')(net2['drop3m'])
+        net2['dense2m'] = Dense(5, name='prediction_branch', activation='softmax')(net2['drop3m'])
 
-        model = Model(net2['input_m'], net2['dense2m'])
+        net2['predictions'] = concatenate([net2['dense2m'],
+                                          att['softmax']],
+                                         axis=1, name='predictions')
+
+        model = Model(inputs=net2['input_m'], outputs=net2['predictions'])
 
         
         return model
