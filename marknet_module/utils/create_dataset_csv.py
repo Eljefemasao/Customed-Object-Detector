bec3d40585c3da817989c99f2d15a08981851be3
m.matsunaga <castle2164m@gmail.com>
2020-12-08 20:44:54 +0900

    clean up code

--- marknet_module/utils/create_dataset_csv.py
+++ marknet_module/utils/create_dataset_csv.py
@@ -5,6 +5,8 @@ import pandas as pd
 import glob 
 
 
+
+
 def get_imagepath_and_label_df(data_path):
     """訓練・検証用画像データのpathを取得し、ラベルのannotationを行ってdfを生成
     """    
@@ -12,8 +14,15 @@ def get_imagepath_and_label_df(data_path):
     image_path = glob.glob(data_path+'*.jpg')
     # 画像データのラベル
     label_name = data_path.split('/')[-2]
-
-    image_label = [label_name for _ in range(len(image_path))]
+    label = 0
+    if label_name == 'plateA':
+        label = 0
+    elif label_name == 'plateB':
+        label = 1 
+    elif label_name == 'plateC':
+        label = 2
+     
+    image_label = [label for _ in range(len(image_path))]
     df = pd.DataFrame({'image':image_path, 'label':image_label})
     return df
 
@@ -37,13 +46,73 @@ def write_out_dataset_csv(data_path, output_path):
 
 if __name__ == '__main__':
 
-    # 訓練データまでのpath
-    data_path = ['/Users/matsunagamasaaki/MasterResearch/cup_annotation/mark1/data/cupA/'\
-        ,'/Users/matsunagamasaaki/MasterResearch/cup_annotation/mark2/data/cupB/']
-    # csvファイルの書き出し先
-    output_path = '/Users/matsunagamasaaki/MasterResearch/cup_annotation/mark1/data/data.csv'
+    DATA_DIRECTORY = 'museumSquarePlate_annotation'
 
+    # 訓練データまでのpath
+    """
+    data_path = ['/Users/matsunagamasaaki/MasterResearch/cup_annotation/data2/data/cupA/',
+    '/Users/matsunagamasaaki/MasterResearch/cup_annotation/data2/data/cupB/',
+    '/Users/matsunagamasaaki/MasterResearch/cup_annotation/data5/data/cupB/',
+    '/Users/matsunagamasaaki/MasterResearch/cup_annotation/data6/data/cupA/',
+    '/Users/matsunagamasaaki/MasterResearch/cup_annotation/data8/data/cupA/',
+    '/Users/matsunagamasaaki/MasterResearch/cup_annotation/data8/data/cupB/',
+    '/Users/matsunagamasaaki/MasterResearch/cup_annotation/data10/data/cupA/',
+    '/Users/matsunagamasaaki/MasterResearch/cup_annotation/data10/data/cupB/',
+    '/Users/matsunagamasaaki/MasterResearch/cup_annotation/data11/data/cupA/',
+    '/Users/matsunagamasaaki/MasterResearch/cup_annotation/data11/data/cupB/',
+    '/Users/matsunagamasaaki/MasterResearch/cup_annotation/data12/data/cupA/',
+    '/Users/matsunagamasaaki/MasterResearch/cup_annotation/data12/data/cupB/']
+
+    #['/Users/matsunagamasaaki/MasterResearch/cup_annotation/mark3/data/cupA/'
+    ,'/Users/matsunagamasaaki/MasterResearch/cup_annotation/mark4/data/cupB/']
+
+    # csvファイルの書き出し先
+    output_path = '/Users/matsunagamasaaki/MasterResearch/cup_annotation/mark1/data/fullimage_data.csv'
+    """
+
+    """
+    data_path=[
+    '/Users/matsunagamasaaki/MasterResearch/museumPlate_annotation/data1/data/plateA/',
+    '/Users/matsunagamasaaki/MasterResearch/museumPlate_annotation/data1/data/plateB/',
+    '/Users/matsunagamasaaki/MasterResearch/museumPlate_annotation/data1/data/plateC/',    
+    '/Users/matsunagamasaaki/MasterResearch/museumPlate_annotation/data2/data/plateA/',
+    '/Users/matsunagamasaaki/MasterResearch/museumPlate_annotation/data2/data/plateB/',
+    '/Users/matsunagamasaaki/MasterResearch/museumPlate_annotation/data2/data/plateC/',
+    '/Users/matsunagamasaaki/MasterResearch/museumPlate_annotation/data3/data/plateA/',
+    '/Users/matsunagamasaaki/MasterResearch/museumPlate_annotation/data3/data/plateB/'
+    ,'/Users/matsunagamasaaki/MasterResearch/museumPlate_annotation/data3/data/plateC/']
+    """
+
+
+    data_path=[ '/Users/matsunagamasaaki/MasterResearch/'+DATA_DIRECTORY+'/data4/data/plateA/',
+                '/Users/matsunagamasaaki/MasterResearch/'+DATA_DIRECTORY+'/data4/data/plateB/',
+                '/Users/matsunagamasaaki/MasterResearch/'+DATA_DIRECTORY+'/data4/data/plateC/']
+
+    output_path = '/Users/matsunagamasaaki/MasterResearch/'+DATA_DIRECTORY+'/data4/data/image_data.csv'
+
+
+    """
+    remote_data_path = ['/tmp/museumPlate_annotation/data2/data/plateA/',
+                         '/tmp/museumPlate_annotation/data2/data/plateB/',
+                         '/tmp/museumPlate_annotation/data2/data/plateC/',
+                         '/tmp/museumPlate_annotation/data3/data/plateA/',
+                         '/tmp/museumPlate_annotation/data3/data/plateB/',
+                         '/tmp/museumPlate_annotation/data3/data/plateC/']
+    """
+
+    remote_data_path = ['/tmp/'+DATA_DIRECTORY+'/data4/data/plateA/',
+                        '/tmp/'+DATA_DIRECTORY+'/data4/data/plateB/',
+                        '/tmp/'+DATA_DIRECTORY+'/data4/data/plateC/']
+
+
+
+    remote_output_path = '/tmp/'+DATA_DIRECTORY+'/data4/data/image_data.csv'
+
+    # ローカル
     write_out_dataset_csv(data_path, output_path)
+
+    # リモート
+#    write_out_dataset_csv(remote_data_path, remote_output_path)
     
 
 
