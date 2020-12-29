0afd7a4c00d357165ae5c81f719f91fdc01aa59b
m.matsunaga <castle2164m@gmail.com>
2020-12-29 12:07:35 +0900

    ad basenet model

--- marknet_module/model/MarkNet_keras.py
+++ marknet_module/model/MarkNet_keras.py
@@ -21,13 +21,61 @@ import numpy as np
 
 from utils.white_attenuation_layer import WhiteAttenuation
 
-
+from keras.applications.inception_v3 import InceptionV3
+from keras.applications.vgg16 import VGG16
+from keras.applications.resnet50 import ResNet50
 
 
 class Model_:
 
     def __init__(self, input_shape):
         self.input_shape = input_shape
+
+    def Inceptionv3(self):
+
+        base_model = InceptionV3(
+            include_top=False,
+            weights="imagenet",
+            input_shape=self.input_shape
+        )
+        x = base_model.output
+        x = GlobalAveragePooling2D()(x)
+        x = Dense(1024, activation='relu')(x)
+        predictions = Dense(10, activation='softmax')(x)
+
+        model = Model(inputs=base_model.input, outputs=predictions)
+
+        for layer in model.layers[:249]:
+            layer.trainable = False
+
+            # Batch Normalization の freeze解除
+            if layer.name.startswith('batch_normalization'):
+                layer.trainable = True
+
+        for layer in model.layers[249:]:
+            layer.trainable = True
+
+        return model
+
+    def ResNet(self):
+        resnet = ResNet50(include_top=False, weights='imagenet', input_shape=self.input_shape)
+        top_model = Sequential()
+        top_model.add(Flatten(input_shape=resnet.output_shape[1:]))
+        top_model.add(Dense(10, activation='softmax'))
+        model = Model(input=resnet.input, output=top_model(resnet.output))
+        return model
+
+    def VGG(self):
+        base_model = VGG16(weights='imagenet', include_top=False, input_shape=self.input_shape)
+        x = base_model.output
+        x = GlobalAveragePooling2D()(x)
+        x = Dense(1024, activation='relu')(x)
+        predictions = Dense(10, activation='softmax')(x)
+        model = Model(inputs=base_model.input, outputs=predictions)
+        for layer in base_model.layers:
+            layer.trainable = False
+
+        return model
 
     def MarkNet(self):
 
@@ -50,13 +98,18 @@ class Model_:
                                       name='pool1m')(net2['conv1_2m'])
         net2['drop1m'] = Dropout(0.25,
                                  name='drop1m')(net2['pool1m'])
-        net2['conv2_1m'] = Convolution2D(5, 3, 3,  # kernel was 128 for including to ssd
+        net2['conv2_1m'] = Convolution2D(128, 3, 3,  # kernel was 128 for including to ssd
                                          activation='relu',
                                          border_mode='same',
                                          name='conv2_1m')(net2['drop1m'])
-        net2['pool2m'] = MaxPooling2D((2, 2),
+
+        net2['conv2_2m'] = Convolution2D(256, 3, 3,  # kernel was 128 for including to ssd
+                                         activation='relu',
+                                         border_mode='same',
+                                         name='conv2_2m')(net2['conv2_1m'])
+        net2['pool2m'] = MaxPooling2D((3, 3),
                                       border_mode='same',
-                                      name='pool2m')(net2['conv2_1m'])
+                                      name='pool2m')(net2['conv2_2m'])
         net2['drop2m'] = Dropout(0.25,
                                  name='drop2m')(net2['pool2m'])
         net2['flat1m'] = Flatten(name='flat1m')(net2['drop2m'])
@@ -189,6 +242,11 @@ class Model_:
                                       border_mode='same',
                                       name='pool2m')(net2['conv2_1m'])
 
+        # Feature Map g(x_i)に根元からAttenuation Maskを行う場合
+        #
+        net2['white_attenuation'] = WhiteAttenuation(name='white_attenuation')([net2['input_m'] ,net2['pool2m']])
+        net2['pool2m'] = Multiply(name='attention_multi')([net2['pool2m'], net2['white_attenuation']])
+
         ### Attention Branch
 
         att['batch_norm'] = BatchNormalization(name='batch_norm')(net2['pool2m'])
@@ -214,10 +272,9 @@ class Model_:
         att_map['sigmoid'] = Activation(activation='sigmoid')(att_map['batch_norm'])
 
         ### white attenuation branch
-
-        att_map['white_attenuation'] = WhiteAttenuation(name='white_attenuation')([net2['input_m'] ,att_map['sigmoid']])
-
-        att_map['sigmoid'] = Multiply(name='attention_multi')([att_map['sigmoid'], att_map['white_attenuation']])
+        # 後方Attention Map M(x_i)にAttenuation Maskを行う場合
+#        att_map['white_attenuation'] = WhiteAttenuation(name='white_attenuation')([net2['input_m'] ,att_map['sigmoid']])
+#        att_map['sigmoid'] = Multiply(name='attention_multi')([att_map['sigmoid'], att_map['white_attenuation']])
 
 
         ### Connection
@@ -240,10 +297,13 @@ class Model_:
         net2['drop3m'] = Dropout(0.5,
                                  name='drop3m')(net2['dense1m'])
         net2['dense2m'] = Dense(5, name='prediction_branch', activation='softmax')(net2['drop3m'])
+
+
         net2['predictions'] = concatenate([net2['dense2m'],
                                            att['softmax']],
                                           axis=1, name='predictions')
-        model = Model(inputs=net2['input_m'], outputs=net2['predictions'])
+
+        model = Model(inputs=net2['input_m'], outputs=net2['dense2m'])
 
         return model
 
